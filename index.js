df40183fdac5377f9ff59c397b523287efe2fbde
marcusf <marcusf@spotify.com>
2020-05-09 15:47:54 -0400

    Initial commit.

---
+++ index.js
@@ -0,0 +1,92 @@
+const DEATH_COUNT = "DEATH_COUNT"
+const NEW_COVID_CASE_COUNT = "NEW_COVID_CASE_COUNT"
+const HOSPITALIZED_CASE_COUNT = "HOSPITALIZED_CASE_COUNT"
+
+const parseData = (data) => {
+  console.log(data)
+  let cvd = {}
+  let maxs = { DEATH_COUNT: 0, NEW_COVID_CASE_COUNT: 0, HOSPITALIZED_CASE_COUNT: 0 }
+  const months = {"Mar": 3, "Apr": 4, "May": 5}
+  for (const [key,val] of Object.entries(coviddata)) {
+    let date = key.substr(5)
+    let month = date.substr(0,3)
+    let day = date.substr(4)
+    day = day.length == 1 ? "0" + day : day
+    let newkey = months[month] + "-" + day
+    cvd[newkey] = val
+   
+    console.log(val)
+    maxs.DEATH_COUNT = Math.max(maxs.DEATH_COUNT, Math.max.apply(null, val.map(v => +v[DEATH_COUNT])))
+    maxs.NEW_COVID_CASE_COUNT = Math.max(maxs.NEW_COVID_CASE_COUNT, Math.max.apply(null, val.map(v => +v[NEW_COVID_CASE_COUNT]||0)))
+    maxs.HOSPITALIZED_CASE_COUNT = Math.max(maxs.HOSPITALIZED_CASE_COUNT, Math.max.apply(null, val.map(v => +v[HOSPITALIZED_CASE_COUNT]||0)))
+
+  }
+  return [cvd, maxs]
+}
+
+const makeDay = dateStr => {
+  const date = new Date(dateStr)
+  return `${date.getDate()}/${date.getMonth()}`
+}
+
+const getDay = (cvd, day, type) => {
+  const keys = Object.keys(cvd).sort()
+  const p = cvd[keys[day]]
+  let dates = getLongestSequence(cvd)
+  let fields = p.map(k => ({ "Date": new Date(k['DATE_OF_INTEREST']), "Deaths": +k[type]}))
+  let covered = p.map(k => k['DATE_OF_INTEREST'])
+  for (date of dates) {
+    if (covered.indexOf(date) == -1) {
+      fields.push({"Date": new Date(date), "Deaths": 0})
+    }
+  }
+  return [keys[day], fields.sort((a,b) => { a["Date"]-b["Date"] })]
+}
+
+const getLongestSequence = (data) => {
+  let longest = []
+  for (const [_,obj] of Object.entries(data)) {
+    if (obj.length > longest.length) {
+      longest = obj
+    }
+  }
+  return longest.map(v => v['DATE_OF_INTEREST'])
+}
+
+const setupSlider = (data, maxs) => {
+  const size = Object.keys(coviddata).length-1  
+  const slider = document.querySelector("#whichdate")
+  slider.max = size
+  slider.addEventListener("change", e => {
+    renderChart(data, maxs)
+  })
+  document.querySelectorAll('input[name="mode"]').forEach(radio => radio.addEventListener("change", e => { 
+    renderChart(data, maxs)
+  }))
+}
+
+const renderChart = (data, maxs) => {
+
+  let elem = document.querySelector("svg")
+  if (elem) elem.parentElement.removeChild(elem)
+
+  const day = document.querySelector("#whichdate").value
+
+  const form_type = document.querySelector('input[name="mode"]:checked').value
+  const [label, graph] = getDay(data, day, form_type)
+
+  document.querySelector("#wd_label").innerHTML = `Reporting date ${label}`
+
+  var svg = dimple.newSvg("#graph", 800, 600)
+  var chart = new dimple.chart(svg, graph)
+  chart.addTimeAxis("x", "Date")
+  let yaxis = chart.addMeasureAxis("y", "Deaths")
+  yaxis.overrideMax = Math.ceil(maxs[form_type]/100)*100;
+  chart.addSeries(null, dimple.plot.bar);
+  chart.draw();
+}
+
+let [data, maxs] = parseData(coviddata)
+console.log(maxs)
+setupSlider(data, maxs)
+renderChart(data, maxs)
