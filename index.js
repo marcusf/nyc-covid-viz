f613feca8564825ba1590c284acb71248b54c584
marcusf <marcusf@spotify.com>
2020-05-10 13:43:59 -0400

    Replicating old functionality in the new format.

--- index.js
+++ index.js
@@ -2,87 +2,64 @@ const DEATH_COUNT = "DEATH_COUNT"
 const NEW_COVID_CASE_COUNT = "NEW_COVID_CASE_COUNT"
 const HOSPITALIZED_CASE_COUNT = "HOSPITALIZED_CASE_COUNT"
 
+// {"reporting_date":"2020-03-26","date_of_interest":"2020-03-02","cases":1,
+// "hospitalized":0,"deaths":0,"cases_new":1,"cases_last_report":0,"hospitalized_new":0,
+// "hospitalized_last_report":0,"deaths_new":0,"deaths_last_report":0},
 const parseData = (data) => {
-  dates = Object.keys(data).sort()
-  let maxs = { DEATH_COUNT: 0, NEW_COVID_CASE_COUNT: 0, HOSPITALIZED_CASE_COUNT: 0 }
-  let yesterday = null, yday = null
-  for (const day of Object.keys(data).sort()) {
-    val = data[day]
+  let maxs = { deaths: 0, cases: 0, hospitalized: 0 }
+  let days = Array.from(new Set(data.map(d => d.reporting_date))).sort()
 
-    maxs.DEATH_COUNT = Math.max(maxs.DEATH_COUNT, Math.max.apply(null, val.map(v => +v[DEATH_COUNT])))
-    maxs.NEW_COVID_CASE_COUNT = Math.max(maxs.NEW_COVID_CASE_COUNT, Math.max.apply(null, val.map(v => +v[NEW_COVID_CASE_COUNT]||0)))
-    maxs.HOSPITALIZED_CASE_COUNT = Math.max(maxs.HOSPITALIZED_CASE_COUNT, Math.max.apply(null, val.map(v => +v[HOSPITALIZED_CASE_COUNT]||0)))
-
-    for (let i = 0; i < val.length; i++) {
-      val[i][DEATH_COUNT] = { Previous: 0, New: parseInt(val[i][DEATH_COUNT])||0 }
-      val[i][NEW_COVID_CASE_COUNT] = { Previous: 0, New: parseInt(val[i][NEW_COVID_CASE_COUNT])||0 }
-      val[i][HOSPITALIZED_CASE_COUNT] = { Previous: 0, New: parseInt(val[i][HOSPITALIZED_CASE_COUNT])||0 }
-    }
-
-    if (yesterday != null) {
-      for (let i = 0; i < yesterday.length; i++) {
-        let dp = yesterday[i][DEATH_COUNT].Previous + yesterday[i][DEATH_COUNT].New
-        let cp = yesterday[i][NEW_COVID_CASE_COUNT].Previous + yesterday[i][NEW_COVID_CASE_COUNT].New
-        let hp = yesterday[i][HOSPITALIZED_CASE_COUNT].Previous + yesterday[i][HOSPITALIZED_CASE_COUNT].New
-        for (let j = 0; j < val.length; j++) {
-          if (val[j].DATE_OF_INTEREST == yesterday[i].DATE_OF_INTEREST) {
-            val[j][DEATH_COUNT] = { Previous: dp, New: val[j][DEATH_COUNT].New - dp }
-            val[j][NEW_COVID_CASE_COUNT] = { Previous: cp, New: val[j][NEW_COVID_CASE_COUNT].New - cp }
-            val[j][HOSPITALIZED_CASE_COUNT] = { Previous: hp, New: val[j][HOSPITALIZED_CASE_COUNT].New - hp }
-            break
-          }
-        }
-      }
-    }
-    yesterday = val
-    yday = day
+  for (entry of data) {
+    maxs.deaths = Math.max(maxs.deaths, entry.deaths)
+    maxs.cases = Math.max(maxs.cases, entry.cases)
+    maxs.hospitalized = Math.max(maxs.hospitalized, entry.hospitalized)
   }
-  return [data, maxs]
+
+  return [data, days, maxs]
 }
 
-const getDay = (cvd, day, type) => {
-  const keys = Object.keys(cvd).sort()
-  const p = cvd[keys[day]]
+const getDay = (cvd, days, day, type) => {
   let dates = getLongestSequence(cvd)
+  const p = cvd.filter(l => l.reporting_date == days[day])
 
-  let prev = p.map(k => ({ "Date": k['DATE_OF_INTEREST'], "Type": "Previous", "Count": +k[type].Previous}))
-  let new_ = p.map(k => ({ "Date": k['DATE_OF_INTEREST'], "Type": "New", "Count": +k[type].New}))
+  let prev = p.map(k => ({ "Date": k['date_of_interest'], "Type": "Previous", "Count": +k[type+"_last_report"]}))
+  let new_ = p.map(k => ({ "Date": k['date_of_interest'], "Type": "New", "Count": +k[type+"_new"]}))
+
   let fields = prev.concat(new_)
-  let covered = p.map(k => k['DATE_OF_INTEREST'])
+  let covered = p.map(k => k['date_of_interest'])
   for (date of dates) {
     if (covered.indexOf(date) == -1) {
       fields.push({"Date": date, "Type": "Previous", "Count": 0})
       fields.push({"Date": date, "Type": "New", "Count": 0})
     }
   }
-  return [keys[day], fields.sort((a,b) => { return new Date(a["Date"])-new Date(b["Date"]) })]
+  return [days[day], fields.sort((a,b) => { return new Date(a["Date"])-new Date(b["Date"]) })]
 }
 
 const getLongestSequence = (data) => {
-  let longest = []
-  for (const [_,obj] of Object.entries(data)) {
-    if (obj.length > longest.length) {
-      longest = obj
-    }
+  let output = [], reporting_date = data[data.length-1].reporting_date, i = data.length-1
+  while (data[i].reporting_date == reporting_date) {
+    output.push(data[i].date_of_interest)
+    i--
   }
-  return longest.map(v => v['DATE_OF_INTEREST'])
+  return output.reverse()
 }
 
-const setupSlider = (data, maxs) => {
-  const size = Object.keys(coviddata).length-1  
+const setupSlider = (data, days, maxs) => {
+  const size = days.length
   const slider = document.querySelector("#whichdate")
-  slider.max = size
+  slider.max = size-1
   slider.value = size
   slider.addEventListener("input", e => {
-    renderChart(data, maxs)
+    renderChart(data, days, maxs)
   })
   document.querySelectorAll('input[name="mode"]').forEach(radio => radio.addEventListener("change", e => { 
-    renderChart(data, maxs)
+    renderChart(data, days, maxs)
   }))
   slider.focus()
 }
 
-const renderChart = (data, maxs) => {
+const renderChart = (data, days, maxs) => {
 
   let elem = document.querySelector("svg")
   if (elem) elem.parentElement.removeChild(elem)
@@ -90,7 +67,7 @@ const renderChart = (data, maxs) => {
   const day = document.querySelector("#whichdate").value
 
   const form_type = document.querySelector('input[name="mode"]:checked').value
-  const [label, graph] = getDay(data, day, form_type)
+  const [label, graph] = getDay(data, days, day, form_type)
 
   document.querySelector("#wd_label").innerHTML = `Reporting date ${label}`
 
@@ -102,7 +79,7 @@ const renderChart = (data, maxs) => {
 
   window.addEventListener('resize', () => { chart.draw(0, true) })
 
-  let x = chart.addTimeAxis("x", "Date", "%m/%e/%y", "%d %b")
+  let x = chart.addTimeAxis("x", "Date", "%Y-%m-%d", "%d %b")
   x.addOrderRule("Date")
   let yaxis = chart.addMeasureAxis("y", "Count")
 
@@ -118,6 +95,6 @@ const renderChart = (data, maxs) => {
   chart.draw();
 }
 
-let [data, maxs] = parseData(coviddata)
-setupSlider(data, maxs)
-renderChart(data, maxs)
+let [data, days, maxs] = parseData(coviddata)
+setupSlider(data, days, maxs)
+renderChart(data, days, maxs)
